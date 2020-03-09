9881a23472757e36cb21e463b02489013255fbf7
Davoleo <davoleo2001@gmail.com>
2020-03-09 21:37:13 +0100

    the discord token is now the program argument

--- src/main/java/CurseUpdates.java
+++ src/main/java/CurseUpdates.java
@@ -6,39 +6,16 @@
  * Copyright - © - Davoleo - 2020
  * ----------------------------------- */
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
-import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.User;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 public class CurseUpdates {
 
     public static void main(String[] args) {
 
-        String token = "";
-        ObjectMapper mapper = new ObjectMapper();
-
-        //System.out.println("PATHALCALCSCSS: " + CurseUpdates.class.getResource("").getPath());
-
-        InputStream iStream = CurseUpdates.class.getResourceAsStream("/config.json");
-        try {
-            Token jsonObject = mapper.readValue(iStream, Token.class);
-            token = jsonObject.getToken();
-            System.out.println(jsonObject);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (token.equals(""))
-            return;
-
-        DiscordClientBuilder builder = new DiscordClientBuilder(token);
+        DiscordClientBuilder builder = new DiscordClientBuilder(args[0]);
 
         DiscordClient client = builder.build();
 
