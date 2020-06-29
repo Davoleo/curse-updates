545a25efa016f960d50f2b652b5d3325c1501bb5
Davoleo <davoleo2001@gmail.com>
2020-06-29 19:14:16 +0200

    Revert "Migrate to Typescript + JS minifying"
    This reverts commit aafacfcdac67b61f97c50e304edd5983ba7bf983.

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const config = require('../cfg.json');
+const discord = require('discord.js');
+const client = new discord.Client();
+const { commands } = require('./commands');
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	if(msg.content.startsWith(config.prefix)) {
+		// The Command message trimmed of the prefix
+		const trimmedCommand = msg.content.replace(config.prefix, '');
+		// If the command message is equals to one of the commands in the Map
+		for (const command of commands.keys()) {
+			if(trimmedCommand.indexOf(command) !== -1) {
+				// Invoke the Command Function
+				// console.log('"' + trimmedCommand + '"');
+				commands.get(command)(msg);
+			}
+		}
+	}
+});
+
+client.login(config.token);
--- src/query.js
+++ src/query.js
@@ -1,28 +1,27 @@
 const { Utils } = require('./utils');
+
 const cf = require('mc-curseforge-api');
 
-function queryLatest(id) {
-	cf.getMod(id).then((mod) => {
-		const latestFile = mod.latestFiles[mod.latestFiles.length - 1];
-		const embed = Utils.buildModEmbed(mod, latestFile);
-		return embed;
-	}, (error) => {
-		return 'Error: ' + error + '!';
-	});
-}
-
-function getModById(id) {
-
-	let modInstance = null;
-
-	cf.getMod(id).then((mod) => {
-		modInstance = mod;
-	});
-
-	return modInstance;
-}
-
 exports.cfquery = {
-	queryLatest,
-	getModById,
+
+	queryLatest(id) {
+		cf.getMod(id).then((mod) => {
+			const latestFile = mod.latestFiles[mod.latestFiles.length - 1];
+			const embed = Utils.buildModEmbed(mod, latestFile);
+			return embed;
+		},
+		(error) => {
+			return 'Error: ' + error + '!';
+		});
+	},
+
+	getModById(id) {
+		cf.getMod(id).then((mod) => {
+			return mod;
+		},
+		() => {
+			return null;
+		},
+		);
+	},
 };
