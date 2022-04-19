b49f5ffc9dfab0d535a8eed7d723102640a0598c
davoleo <dav@davoleo.net>
2022-04-19 18:36:13 +0200

    Improve RPC + migrate to Environment in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Snowflake } from 'discord-api-types/globals';
-import { Client, Permissions } from 'discord.js';
+import { Client, ClientUser, Permissions } from 'discord.js';
 import { createWriteStream, existsSync, mkdirSync, WriteStream } from 'fs';
-import * as config from './data/config.json';
+import Environment from './model/Environment';
 
 ///The different levels of permission that may be needed to execute a certain command
 export enum CommandPermission {
@@ -76,27 +76,29 @@ export class Logger {
 	}
 }
 
+const env = Environment.get();
 export class Utils {
 
+
 	static sendDMtoBotOwner(client: Client, message: string): void {
-		client.users.fetch(config.ownerId).then(owner => {
+		client.users.fetch(env.OwnerId).then(owner => {
 			owner.send(message);
-		})
+		});
 	}
 
-	static hasPermission(authorId: Snowflake, discordPermissions: Permissions, permissionLevel: CommandPermission): boolean {
+	static hasPermission(authorId: Snowflake, discordPermissions: Permissions | null, permissionLevel: CommandPermission): boolean {
 
 		//Guild is not available we only check for owner level permission
 		if (discordPermissions === null) {
 			if (permissionLevel == CommandPermission.OWNER)
-				return authorId === config.ownerId;
+				return authorId === env.OwnerId;
 			else
 				return true;
 		}
 
 		switch (permissionLevel) {
 			case CommandPermission.OWNER:
-				return authorId === config.ownerId;
+				return authorId === env.OwnerId;
 			case CommandPermission.ADMINISTRATOR:
 				return discordPermissions.has("MANAGE_GUILD", true) || discordPermissions.has("MANAGE_ROLES", true);
 			case CommandPermission.MODERATOR:
@@ -113,31 +115,17 @@ export class Utils {
 		return splitUrl[splitUrl.length - 1];
 	}
 
-	static updateBotStatus(client: Client, devMode: boolean): void {
+	static updateBotStatus(botUser: ClientUser, devMode: boolean): void {
 		// Set the bot status
-		if(devMode) {
-			client.user.setPresence({
-				status: 'dnd',
-				afk: false,
-				activities: [
-					{
-						name: ' with Davoleo in VSCode',
-						type: 'PLAYING',
-					}
-				],
-			});
-		}
-		else {
-			client.user.setPresence({
-				status: 'online',
-				afk: false,
-				activities: [
-					{
-						name: ' for updates on CF',
-						type: 'WATCHING',
-					}
-				],
-			});
-		}
+		botUser.setPresence({
+			status: devMode ? 'dnd' : 'online',
+			afk: false,
+			activities: [
+				{
+					name: devMode ? ' with Davoleo in VSCode' : ' for updaes on CF',
+					type: devMode ? 'PLAYING' : 'WATCHING',
+				}
+			],
+		});
 	}
 }
