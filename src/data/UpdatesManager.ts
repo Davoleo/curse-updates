2b8ff81798e4198d470d8818e9685ff2c317e30d
davoleo <dav@davoleo.net>
2022-04-19 18:38:11 +0200

    nullability fixes in UpdatesManager

--- src/data/UpdatesManager.ts
+++ src/data/UpdatesManager.ts
@@ -2,9 +2,10 @@ import { AnnouncementsConfig } from "@prisma/client";
 import { Snowflake } from "discord.js";
 import { dbclient } from "./dataHandler";
 
+//TODO add projectFilter to Prisma Schema
 export default class UpdatesManager {
 
-    private constructor(public serverId: Snowflake, public announcementsConfig: AnnouncementsConfig[]) {}
+    private constructor(public serverId: Snowflake, public announcementsConfig: Array<AnnouncementsConfig | null>) {}
 
     static async ofServer(id: Snowflake): Promise<UpdatesManager> {
         const configs = await dbclient.announcementsConfig.findMany({
@@ -34,9 +35,9 @@ export default class UpdatesManager {
                 });
             }
             else {
-                const newChannel = this.announcementsConfig[i].channel;
-                const newMessage = this.announcementsConfig[i].message;
-                const newFilter = this.announcementsConfig[i].gameVerFilter;
+                const newChannel = this.announcementsConfig[i]!.channel;
+                const newMessage = this.announcementsConfig[i]!.message;
+                const newFilter = this.announcementsConfig[i]!.gameVerFilter;
                 await dbclient.announcementsConfig.upsert({
                     where: {
                         id_serverId: {
@@ -59,51 +60,56 @@ export default class UpdatesManager {
             }
         }
     }
+
+    private isTemplateInvalid(tid: number) {
+        return tid < 0 || tid >= this.announcementsConfig.length || this.announcementsConfig[tid] === null;
+    }
     
-    addReportTemplate(channel: Snowflake = undefined, message: string = undefined, gameVerFilter: string = undefined): void {
+    addReportTemplate(channel: Snowflake | undefined = undefined, message: string | undefined = undefined, gameVerFilter: string | undefined = undefined): void {
         //id is -1 because it's just a temporary number to allow adding this object to the inner field
         this.announcementsConfig.push({
             id: this.announcementsConfig.length,
             serverId: this.serverId,
-            channel: channel,
-            message: message,
-            gameVerFilter: gameVerFilter
+            channel: channel ?? null,
+            message: message ?? null,
+            gameVerFilter: gameVerFilter ?? null
         });
     }
 
     removeReportTemplate(tid: number): void {
-        if (tid < 0 || tid >= this.announcementsConfig.length)
+        if (this.isTemplateInvalid(tid))
             throw Error("That Announcement Template doesn't exist!");
 
         this.announcementsConfig[tid] = null;
     }
 
-    editReportChannel(tid: number, channel: Snowflake = null): void {
-        if (tid < 0 || tid >= this.announcementsConfig.length)
+    editReportChannel(tid: number, channel: Snowflake | undefined): void {
+        if (this.isTemplateInvalid(tid))
             throw Error("That Announcement Template doesn't exist!");
         
-        this.announcementsConfig[tid].channel = channel;
+        this.announcementsConfig[tid]!.channel = channel ?? null;
     }
 
-    editReportMessage(tid: number, message: string = null): void {
-        if (tid < 0 || tid >= this.announcementsConfig.length)
+    editReportMessage(tid: number, message: string | undefined): void {
+        if (this.isTemplateInvalid(tid))
             throw Error("That Announcement Template doesn't exist!");
 
-        this.announcementsConfig[tid].message = message;
+        this.announcementsConfig[tid]!.message = message ?? null;
     }
 
     setGameVersionFilter(tid: number, gameVersions: string[]): void {
-        if (tid < 0 || tid >= this.announcementsConfig.length)
+        if (this.isTemplateInvalid(tid))
             throw Error("That Announcement Template doesn't exist!");
 
-        this.announcementsConfig[tid].gameVerFilter = gameVersions.join('|');
+        this.announcementsConfig[tid]!.gameVerFilter = gameVersions.join('|');
     }
 
-    getGameVersionFilter(tid: number): string[] {
-        if (tid < 0 || tid >= this.announcementsConfig.length)
+    getGameVersionFilter(tid: number): string[] | null {
+        if (this.isTemplateInvalid(tid))
             throw Error("That Announcement Template doesn't exist!");
 
-        return this.announcementsConfig[tid].gameVerFilter.split('|');
+        const gameVersString = this.announcementsConfig[tid]!.gameVerFilter;
+        return gameVersString !== null ? gameVersString.split('|') : null;
     }
 
 }
