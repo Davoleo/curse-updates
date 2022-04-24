744e90ba6f227f3f0bfb5b9df2f843f635832011
Davoleo <dav@davoleo.net>
2022-04-24 18:24:32 +0200

    rename field annoucementsConfig -> config

--- src/data/UpdatesManager.ts
+++ src/data/UpdatesManager.ts
@@ -5,7 +5,7 @@ import { dbclient } from "./dataHandler";
 
 export default class UpdatesManager {
 
-    private constructor(public serverId: Snowflake, public announcementsConfig: Array<AnnouncementsConfig | null>) {}
+    private constructor(public serverId: Snowflake, public config: Array<AnnouncementsConfig | null>) {}
 
     static async ofServer(id: Snowflake): Promise<UpdatesManager> {
         const configs = await dbclient.announcementsConfig.findMany({
@@ -22,8 +22,8 @@ export default class UpdatesManager {
 
     async save() {
         // Update Announcement Templates
-        for (const i of this.announcementsConfig.keys()) {
-            if (this.announcementsConfig[i] === null) {
+        for (const i of this.config.keys()) {
+            if (this.config[i] === null) {
                 //Remove Annoucement Template
                 await dbclient.announcementsConfig.delete({
                     where: {
@@ -35,10 +35,10 @@ export default class UpdatesManager {
                 });
             }
             else {
-                const newChannel = this.announcementsConfig[i]!.channel;
-                const newMessage = this.announcementsConfig[i]!.message;
-                const newGamesFilter = this.announcementsConfig[i]!.gameVerFilter;
-                const newProjectsFilter = this.announcementsConfig[i]!.projectsFilter;
+                const newChannel = this.config[i]!.channel;
+                const newMessage = this.config[i]!.message;
+                const newGamesFilter = this.config[i]!.gameVerFilter;
+                const newProjectsFilter = this.config[i]!.projectsFilter;
                 await dbclient.announcementsConfig.upsert({
                     where: {
                         id_serverId: {
@@ -64,13 +64,13 @@ export default class UpdatesManager {
     }
 
     isTemplateInvalid(tid: number) {
-        return tid < 0 || tid >= this.announcementsConfig.length || this.announcementsConfig[tid] === null;
+        return tid < 0 || tid >= this.config.length || this.config[tid] === null;
     }
     
     addReportTemplate(channel: Snowflake | undefined = undefined, message: string | undefined = undefined): void {
         //id is -1 because it's just a temporary number to allow adding this object to the inner field
-        this.announcementsConfig.push({
-            id: this.announcementsConfig.length,
+        this.config.push({
+            id: this.config.length,
             serverId: this.serverId,
             channel: channel ?? null,
             message: message ?? null,
@@ -81,40 +81,40 @@ export default class UpdatesManager {
 
     removeReportTemplate(tid: number): void {
         assert(!this.isTemplateInvalid(tid));
-        this.announcementsConfig[tid] = null;
+        this.config[tid] = null;
     }
 
     editReportChannel(tid: number, channel: Snowflake | undefined): void {
         assert(!this.isTemplateInvalid(tid));
-        this.announcementsConfig[tid]!.channel = channel ?? null;
+        this.config[tid]!.channel = channel ?? null;
     }
 
     editReportMessage(tid: number, message: string | undefined): void {
         assert(!this.isTemplateInvalid(tid));
-        this.announcementsConfig[tid]!.message = message ?? null;
+        this.config[tid]!.message = message ?? null;
     }
 
     setGameVersionFilter(tid: number, gameVersions: string[] | undefined): void {
         assert(!this.isTemplateInvalid(tid));
-        this.announcementsConfig[tid]!.gameVerFilter = gameVersions !== undefined ? gameVersions.join('|') : null;
+        this.config[tid]!.gameVerFilter = gameVersions !== undefined ? gameVersions.join('|') : null;
     }
 
     getGameVersionFilter(tid: number): string[] | undefined {
         assert(!this.isTemplateInvalid(tid));
-        const gameVersString = this.announcementsConfig[tid]!.gameVerFilter;
+        const gameVersString = this.config[tid]!.gameVerFilter;
         
         return gameVersString !== null ? gameVersString.split('|') : undefined;
     }
 
     setProjectsFilter(tid: number, slugs: string[] | undefined): void {
         assert(!this.isTemplateInvalid(tid));
-        this.announcementsConfig[tid]!.projectsFilter = slugs !== undefined ? slugs.join('|') : null;
+        this.config[tid]!.projectsFilter = slugs !== undefined ? slugs.join('|') : null;
     }
 
     getProjectsFilter(tid: number): string[] | undefined {
         assert(!this.isTemplateInvalid(tid));
 
-        const slugString = this.announcementsConfig[tid]!.projectsFilter;
+        const slugString = this.config[tid]!.projectsFilter;
         return slugString !== null ? slugString.split('|') : undefined;
     }
 
