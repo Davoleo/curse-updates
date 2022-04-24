de1e1cc937d8c786f161501504148408fc0c1df8
Davoleo <dav@davoleo.net>
2022-04-24 18:23:38 +0200

    Remove useless function from dataHandler

--- src/data/dataHandler.ts
+++ src/data/dataHandler.ts
@@ -1,13 +1,9 @@
-import { PrismaClient, ServerConfig } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 export const dbclient = new PrismaClient({
     log: ["query", "info", "warn", "error"]
 });
 
-async function getAllServerConfigs(): Promise<ServerConfig[]> {
-    return await dbclient.serverConfig.findMany();
-}
-
 export const DBHelper = {
-    getAllServerConfigs
+    
 }
