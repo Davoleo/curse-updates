02befdb9786d4d7bf83ce610660be1b7e8418d5b
Davoleo <dav@davoleo.net>
2022-05-19 19:10:57 +0200

    redirect prisma logs

--- src/data/dataHandler.ts
+++ src/data/dataHandler.ts
@@ -1,9 +1,45 @@
 import { PrismaClient } from "@prisma/client";
+import { logger } from "../main";
 
 export const dbclient = new PrismaClient({
-    log: ["query", "info", "warn", "error"]
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'event',
+            level: 'info',
+        },
+        {
+            emit: 'event',
+            level: 'warn',
+        },
+        {
+            emit: 'event',
+            level: 'error',
+        }
+    ]
 });
 
+function init() {
+    dbclient.$on('query', (event) => {
+        logger.info('Query: ' + event.query);
+        logger.info('Params: ' + event.params);
+        logger.info('Duration: ' + event.duration + 'ms');
+    });
+
+    dbclient.$on('info', (event) => {
+        logger.info(event.message);
+    });
+    dbclient.$on('warn', (event) => {
+        logger.warn(event.message);
+    });
+    dbclient.$on('error', (event) => {
+        logger.error(event.message);
+    });
+}
+
 export const DBHelper = {
-    
+    init
 }
