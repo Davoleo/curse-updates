1a127205f051a2abb314ac46b0b0bdb44866758e
Davoleo <dav@davoleo.net>
2023-07-19 19:31:48 +0200

    Added Validator to GameTag

--- src/model/GameTag.ts
+++ src/model/GameTag.ts
@@ -1,13 +1,26 @@
+import { CurseHelper } from "../curseHelper";
+
 export default class GameTag {
-    constructor(public game: number, public tag: string) {}
+    constructor(public game: string, public tag: string) {}
 
     join(): string {
         return `${this.game}:${this.tag}`
     }
 
+    validate(): void {
+        const gameId = CurseHelper.GameSlugs.get(this.game)
+
+        if (!gameId)
+            throw Error("Invalid game: " + this.game + "!")
+
+        const versions = CurseHelper.GameVersions.get(gameId)
+        if (!versions || !versions.has(this.tag))
+            throw Error("Invalid version: " + this.tag + "!")
+    }
+
     static fromString(serialized: string): GameTag {
         const separator = serialized.indexOf(':');
-        const game = Number(serialized.substring(0, separator));
+        const game = serialized.substring(0, separator);
         return new GameTag(game, serialized.substring(separator + 1));
     }
 }
